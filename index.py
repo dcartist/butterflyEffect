86501ef261462224fd224ec7ebb56a54f9606870
DCartist <pbannerman13@gmail.com>
2019-09-05 19:23:33 -0400

    committing

---
+++ index.py
@@ -0,0 +1,26 @@
+
+import matplotlib.pyplot as plt
+# plt.plot([1, 2, 3, 6])
+x = list(range(-6, 7))
+print(x)
+y=[]
+
+for i in x:
+    print (i)
+    y.append(i*i*i)
+
+print(y)
+    
+# plt.plot([1, 2, 3, 5], [1, 6, 9, 10], 'ro')
+
+
+# plt.plot(x, y, 'ro')
+# plt.axis([-6, 6, 0, 36])
+# plt.ylabel('some numbers')
+# plt.show()
+
+
+
+# Main Code
+# xnew=sin(y*b)+c*sin(x*b)
+# ynew=sin(x*a)+d*sin(y*a)
