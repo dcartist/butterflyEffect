9e5675b883da39a5187be6b6eedc210cfa70fa2d
DCartist <pbannerman13@gmail.com>
2019-09-05 19:32:50 -0400

    linspace works

--- index.py
+++ index.py
@@ -1,16 +1,22 @@
 
 import matplotlib.pyplot as plt
+import numpy as np
+
+
 # plt.plot([1, 2, 3, 6])
-x = list(range(-6, 7))
-print(x)
-y=[]
+# x = list(range(-6, 7))
+z = np.linspace(-0.1,1,20)
+# print(x)
+print(z)
+# y=[]
 
-for i in x:
-    print (i)
-    y.append(i*i*i)
+# for i in x:
+#     print (i)
+#     y.append(i*i*i)
 
-print(y)
+# print(y)
     
+# plt.plot([1, 2, 3, 5], [1, 6, 9, ], 'ro')
 # plt.plot([1, 2, 3, 5], [1, 6, 9, 10], 'ro')
 
 
